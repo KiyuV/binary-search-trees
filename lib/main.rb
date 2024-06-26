bce2df5840011fed0142e1a64b6ba715f48b3977
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-06-26 16:08:55 +0200

    feat: implement #insert and #insert_rec

--- lib/main.rb
+++ lib/main.rb
@@ -6,9 +6,17 @@ require './lib/tree'
 class Main
   def initialize
     @tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+    #@tree = Tree.new([])
   end
 
   def run
+    @tree.insert(6345)
+    @tree.insert(1)
+    @tree.insert(3)
+    @tree.insert(25)
+    @tree.insert(22)
+    @tree.insert(2)
+    @tree.insert(323)
     @tree.pretty_print
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -7,10 +7,14 @@ class Tree
     @root = build_tree(array)
   end
 
-  def pretty_print(node = @root, prefix = '', is_left: true)
+  def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value)
+    @root = insert_rec(@root, value)
   end
 
   private
@@ -18,6 +22,8 @@ class Tree
   def build_tree(array)
     sorted_array = array.sort.uniq!
     first = 0
+    return nil if array.empty?
+
     last = sorted_array.length - 1
 
     balanced_bst(sorted_array, first, last)
@@ -29,8 +35,24 @@ class Tree
     mid_point = (first + last) / 2
     root = Node.new(array[mid_point])
 
-    root.left = balanced_BST(array, first, mid_point - 1)
-    root.right = balanced_BST(array, mid_point + 1, last)
+    root.left = balanced_bst(array, first, mid_point - 1)
+    root.right = balanced_bst(array, mid_point + 1, last)
+
+    root
+  end
+
+  def insert_rec(root, value)
+    return root = Node.new(value) if root.nil?
+
+    if value > root.data
+      root.right = insert_rec(root.right, value)
+    elsif value < root.data
+      root.left = insert_rec(root.left, value)
+    # prevent duplicate nodes from being insterted
+    else
+      puts "#{value} already exists"
+      root
+    end
 
     root
   end
